7b78d0c29b93faa2db8c80853ca4df4899ecc1ba
NovaCookie <95030874+NovaCookie@users.noreply.github.com>
2024-12-31 00:22:47 +0100

    fix aperçus non aperçus

--- src/components/Contents/Cards/ProjectsCard.jsx
+++ src/components/Contents/Cards/ProjectsCard.jsx
@@ -7,25 +7,25 @@ export default function ProjectsCard({ image, alt, title, text, githubLink, aper
         <div className="col-12 col-md-6 col-lg-4 mb-4">
             <div className="card project h-100">
                 <div className="card-body">
-                    <Image 
-                    className="img-fluid" 
-                    src={`/images/${image}`} 
-                    alt={alt} 
-                    width={400}  
-                    height={200}
-                    layout='responsive'
+                    <Image
+                        className="img-fluid"
+                        src={`/images/${image}`}
+                        alt={alt}
+                        width={400}
+                        height={200}
+                        layout='responsive'
                     />
                     <h3 className="card-title text-center mt-3">{title}</h3>
                     <p className="card-text">{text}</p>
                 </div>
                 <div className="card-footer">
                     <div className="row">
-                        <div className="col-6">
-                        <Link href={githubLink} className="btn">Github</Link>
+                        <div className= {aperçusLink ? "col-6" : "col-12"}>
+                            <Link href={githubLink} className="btn">Github</Link>
                         </div>
-                        <div className="col-6">
-                        <Link href={aperçusLink} className="btn">Aperçu</Link>
-                        </div>
+                        {aperçusLink ? <div className="col-6">
+                            <Link href={aperçusLink} className="btn">Aperçu</Link>
+                        </div> : null}
                     </div>
                 </div>
             </div>
